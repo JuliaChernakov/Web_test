1b81a6fcc8e4f5aa5069b8a6a83ceb08186e7f51
Julia Chernakov <julia.chernakov@gmail.com>
2022-05-26 04:18:39 +0300

    refactor webtests

--- src/test/java/WebTest.java
+++ src/test/java/WebTest.java
@@ -9,11 +9,13 @@ import java.util.Locale;
 
 public class WebTest {
 
-    //1. Открыть страницу http://www.99-bottles-of-beer.net/
-    //2. Нажать пункт меню Browse Languages
-    //3. Нажать пункт меню Start
-    //4. Подтвердить, что пользователь видит заголовок "Welcome to 99 Bottles of Beer"
-    //5. Закрыть браузер
+    /**
+     * 1. Открыть страницу http://www.99-bottles-of-beer.net/
+     * 2. Нажать пункт меню Browse Languages
+     * 3. Нажать пункт меню Start
+     * 4. Подтвердить, что пользователь видит заголовок "Welcome to 99 Bottles of Beer"
+     * 5. Закрыть браузер
+     */
 
     @Test
     public void testMenuStartTitle() throws InterruptedException {
@@ -47,8 +49,10 @@ public class WebTest {
         driver.quit();
     }
 
-    //    Подтвердите, что на странице по базовой ссылке в правом верхнем
-//    углу пользователь видит заголовок 99 Bottles of Beer
+    /**
+     * Подтвердите, что на странице по базовой ссылке в правом верхнем
+     * углу пользователь видит заголовок 99 Bottles of Beer
+     */
     @Test
     public void testHeader() {
 
@@ -71,15 +75,17 @@ public class WebTest {
         driver.quit();
     }
 
-    //    Подтвердите, что на странице по базовой ссылке
-//    последний пункт меню называется Submit new Language
+    /**
+     * Подтвердите, что на странице по базовой ссылке
+     * последний пункт меню называется Submit new Language
+     */
     @Test
     public void testLastMenuItem() {
 
         String chromeDriver = "webdriver.chrome.driver";
         String driverPath = "/Applications/chromedriver";
         String url = "http://www.99-bottles-of-beer.net/";
-        String expectedResult = "SUBMIT NEW LANGUAGE";
+        String expectedResult = "Submit New Language";
 
         System.setProperty(chromeDriver, driverPath);
         WebDriver driver = new ChromeDriver();
@@ -87,16 +93,18 @@ public class WebTest {
         driver.get(url);
 
         WebElement lastMenuItem = driver.findElement(
-                By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/submitnewlanguage.html']"));
+                By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li[last()]"));
         String actualResult = lastMenuItem.getText();
 
-        Assert.assertEquals(actualResult, expectedResult);
+        Assert.assertEquals(actualResult, expectedResult.toUpperCase());
 
         driver.quit();
     }
 
-    //    Подтвердите, что на странице по базовой ссылке
-//    последний пункт меню имеет подзаголовок Submit new Language
+    /**
+     * Подтвердите, что на странице по базовой ссылке
+     * последний пункт меню имеет подзаголовок Submit new Language
+     */
     @Test
     public void testLastSubtitle() {
 
@@ -123,8 +131,10 @@ public class WebTest {
         driver.quit();
     }
 
-    //    Подтвердите, что на странице по ссылке http://www.99-bottles-of-beer.net/abc.html ,
-//    первый пункт подменю называется 0-9
+    /**
+     * Подтвердите, что на странице по ссылке http://www.99-bottles-of-beer.net/abc.html ,
+     * первый пункт подменю называется 0-9
+     */
     @Test
     public void testFirstSubmenu() {
 
@@ -147,10 +157,15 @@ public class WebTest {
         driver.quit();
     }
 
-    //    Подтвердите, что имена создателей сайта:
-//    Oliver Schade, Gregor Scheithauer, Stefan Scheler
+    /**
+     * Подтвердите, что имена создателей сайта:
+     * Oliver Schade, Gregor Scheithauer, Stefan Scheler
+     */
+    /**
+     * Option 1 (String concatenation)
+     */
     @Test
-    public void testTeamNames() {
+    public void testTeamNames1() {
 
         String chromeDriver = "webdriver.chrome.driver";
         String driverPath = "/Applications/chromedriver";
@@ -185,7 +200,89 @@ public class WebTest {
         driver.quit();
     }
 
-    // Проверить что в первой строке раздела History упоминается Tim Robinson
+    /**
+     * Option 2 (Three asserts)
+     */
+    @Test
+    public void testTeamNames2() {
+
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "/Applications/chromedriver";
+        String url = "http://www.99-bottles-of-beer.net/";
+        String expectedResult1 = "Oliver Schade";
+        String expectedResult2 = "Gregor Scheithauer";
+        String expectedResult3 = "Stefan Scheler";
+
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(url);
+
+        WebElement menuTeam = driver.findElement(
+                By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='submenu']/li/a[@href='team.html']"));
+        menuTeam.click();
+
+        WebElement os = driver.findElement(
+                By.xpath("//body/div[@id='wrap']/div[@id='main']/h3[1]"));
+        String actualResult1 = os.getText();
+
+        WebElement gs = driver.findElement(
+                By.xpath("//body/div[@id='wrap']/div[@id='main']/h3[2]"));
+        String actualResult2 = gs.getText();
+
+        WebElement ss = driver.findElement(
+                By.xpath("//body/div[@id='wrap']/div[@id='main']/h3[3]"));
+        String actualResult3 = ss.getText();
+
+        Assert.assertEquals(actualResult1, expectedResult1);
+        Assert.assertEquals(actualResult2, expectedResult2);
+        Assert.assertEquals(actualResult3, expectedResult3);
+
+        driver.quit();
+    }
+
+    /**
+     * Option3 (Array)
+     */
+    @Test
+    public void testTeamNames3() {
+
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "/Applications/chromedriver";
+        String url = "http://www.99-bottles-of-beer.net/";
+        String[] expectedResult = {"Oliver Schade", "Gregor Scheithauer", "Stefan Scheler"};
+
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(url);
+
+        WebElement menuTeam = driver.findElement(
+                By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='submenu']/li/a[@href='team.html']"));
+        menuTeam.click();
+
+        String[] actualResult = new String[3];
+
+        WebElement os = driver.findElement(
+                By.xpath("//body/div[@id='wrap']/div[@id='main']/h3[1]"));
+        actualResult[0] = os.getText();
+
+        WebElement gs = driver.findElement(
+                By.xpath("//body/div[@id='wrap']/div[@id='main']/h3[2]"));
+        actualResult[1] = gs.getText();
+
+        WebElement ss = driver.findElement(
+                By.xpath("//body/div[@id='wrap']/div[@id='main']/h3[3]"));
+        actualResult[2] = ss.getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+
+        driver.quit();
+    }
+
+    /**
+     * Проверить что в первой строке раздела History упоминается Tim Robinson
+     */
     @Test
     public void testCreator() {
 
@@ -214,11 +311,13 @@ public class WebTest {
         driver.quit();
     }
 
-    //    Подтвердите, что если на странице по ссылке
-//    http://www.99-bottles-of-beer.net/submitnewlanguage.html ,
-//    пользователь нажмет кнопку Submit Language,
-//    не заполнив информацию в обязательных полях, будет показана ошибка
-//    Error: Precondition failed - Incomplete Input.
+    /**
+     * Подтвердите, что если на странице по ссылке
+     * http://www.99-bottles-of-beer.net/submitnewlanguage.html ,
+     * пользователь нажмет кнопку Submit Language,
+     * не заполнив информацию в обязательных полях, будет показана ошибка
+     * Error: Precondition failed - Incomplete Input.
+     */
     @Test
     public void testSubmitLanguageError() {
 
@@ -251,14 +350,16 @@ public class WebTest {
         driver.quit();
     }
 
-//    Подтвердите, что если на странице по ссылке
-//    http://www.99-bottles-of-beer.net/submitnewlanguage.html ,
-//    пользователь нажмет кнопку Submit Language,
-//    не заполнив информацию в обязательных полях, будет показана ошибка с текстом
-//    Error: Precondition failed - Incomplete Input.
-//    Подтвердите, что в тексте ошибки слова Error, Precondition, Incomplete и Input
-//    написаны с большой буквы, а слово failed написано с маленькой буквы.
-//    Так же подтвердите, что в тексте ошибки содержатся знаки :, -  и .
+    /**
+     * Подтвердите, что если на странице по ссылке
+     * http://www.99-bottles-of-beer.net/submitnewlanguage.html ,
+     * пользователь нажмет кнопку Submit Language,
+     * не заполнив информацию в обязательных полях, будет показана ошибка с текстом
+     * Error: Precondition failed - Incomplete Input.
+     * Подтвердите, что в тексте ошибки слова Error, Precondition, Incomplete и Input
+     * написаны с большой буквы, а слово failed написано с маленькой буквы.
+     * Так же подтвердите, что в тексте ошибки содержатся знаки :"," "-" "."
+     */
 
     @Test
     public void testSubmitLanguageErrorText() {
@@ -266,9 +367,14 @@ public class WebTest {
         String chromeDriver = "webdriver.chrome.driver";
         String driverPath = "/Applications/chromedriver";
         String url = "http://www.99-bottles-of-beer.net/submitnewlanguage.html";
-        String expectedResult1 = "Error: Precondition failed - Incomplete Input.";
-        boolean expectedResult2 = true;
-        boolean expectedResult3 = true;
+        String expectedResult1 = "E";
+        String expectedResult2 = "P";
+        String expectedResult3 = "I";
+        String expectedResult4 = "I";
+        String expectedResult5 = "f";
+        String expectedResult6 = ":";
+        String expectedResult7 = "-";
+        String expectedResult8 = ".";
 
         System.setProperty(chromeDriver, driverPath);
         WebDriver driver = new ChromeDriver();
@@ -281,35 +387,38 @@ public class WebTest {
 
         WebElement error = driver.findElement(
                 By.xpath("//body/div[@id='wrap']/div[@id='main']/p"));
-        String actualResult1 = error.getText();
+        String errorMessage = error.getText();
 
-        boolean actualResult2 = false;
-        if (actualResult1.contains("Error")
-                && actualResult1.contains("Precondition")
-                && actualResult1.contains("Incomplete")
-                && actualResult1.contains("Input")
-                && actualResult1.contains("failed")) {
-            actualResult2 = true;
-        }
-
-        boolean actualResult3 = false;
-        if (actualResult1.contains(":") && actualResult1.contains("-") && actualResult1.contains(".")) {
-            actualResult3 = true;
-        }
+        String[] arrayOfWords = errorMessage.split(" ");
+        String actualResult1 = String.valueOf(arrayOfWords[0].charAt(0));
+        String actualResult2 = String.valueOf(arrayOfWords[1].charAt(0));;
+        String actualResult3 = String.valueOf(arrayOfWords[4].charAt(0));;
+        String actualResult4 = String.valueOf(arrayOfWords[5].charAt(0));;
+        String actualResult5 = String.valueOf(arrayOfWords[2].charAt(0));;
+        String actualResult6 = String.valueOf(arrayOfWords[0].charAt(arrayOfWords[0].length() - 1));
+        String actualResult7 = arrayOfWords[3];
+        String actualResult8 = String.valueOf(arrayOfWords[5].charAt(arrayOfWords[0].length() - 1));
 
         Assert.assertEquals(actualResult1, expectedResult1);
         Assert.assertEquals(actualResult2, expectedResult2);
         Assert.assertEquals(actualResult3, expectedResult3);
+        Assert.assertEquals(actualResult4, expectedResult4);
+        Assert.assertEquals(actualResult5, expectedResult5);
+        Assert.assertEquals(actualResult6, expectedResult6);
+        Assert.assertEquals(actualResult7, expectedResult7);
+        Assert.assertEquals(actualResult8, expectedResult8);
 
         driver.quit();
     }
 
-    //    Подтвердите, что на странице по ссылке
-//    http://www.99-bottles-of-beer.net/submitnewlanguage.html
-//    в первом пункте списка пользователь видит текст
-//    IMPORTANT: Take your time! The more carefully you fill out this form (especially the language name
-//    and description), the easier it will be for us and the faster your language will show up on this page.
-//    We don't have the time to mess around with fixing your descriptions etc. Thanks for your understanding.
+    /**
+     * Подтвердите, что на странице по ссылке
+     * http://www.99-bottles-of-beer.net/submitnewlanguage.html
+     * в первом пункте списка пользователь видит текст
+     * IMPORTANT: Take your time! The more carefully you fill out this form (especially the language name
+     * and description), the easier it will be for us and the faster your language will show up on this page.
+     * We don't have the time to mess around with fixing your descriptions etc. Thanks for your understanding.
+     */
     @Test
     public void testFirstPoint() {
 
@@ -335,8 +444,10 @@ public class WebTest {
         driver.quit();
     }
 
-    //    Подтвердите, что нажав на пункт меню Browse Languages, пользователь увидит таблицу
-//    со следующими названиями для первого и второго столбцов: Language Author
+    /**
+     * Подтвердите, что нажав на пункт меню Browse Languages, пользователь увидит таблицу
+     * со следующими названиями для первого и второго столбцов: Language Author
+     */
     @Test
     public void testTableNames() {
 
@@ -369,8 +480,10 @@ public class WebTest {
         driver.quit();
     }
 
-    //    Подтвердите, что на странице по базовой ссылке пользователь НЕ увидит
-//    новые комментарии, если нажмет на пункты меню Top List → New Comments
+    /**
+     * Подтвердите, что на странице по базовой ссылке пользователь НЕ увидит
+     * новые комментарии, если нажмет на пункты меню Top List → New Comments
+     */
     @Test
     public void testComments() {
 
@@ -401,10 +514,12 @@ public class WebTest {
         driver.quit();
     }
 
-//    Подтвердите, что на странице по ссылке
-//    http://www.99-bottles-of-beer.net/submitnewlanguage.html
-//    пользователь видит предупреждение IMPORTANT:, написанное
-//    белыми буквами bold шрифтом на красном фоне, и что все буквы - capital
+    /**
+     * Подтвердите, что на странице по ссылке
+     * http://www.99-bottles-of-beer.net/submitnewlanguage.html
+     * пользователь видит предупреждение IMPORTANT:, написанное
+     * белыми буквами bold шрифтом на красном фоне, и что все буквы - capital
+     */
     @Test
     public void testImportant() {
 
