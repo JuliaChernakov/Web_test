43d7f906f25c51e967a3042fefb60a40c4185f9c
Julia Chernakov <julia.chernakov@gmail.com>
2022-06-05 04:18:03 +0300

    code refactored and optimised

--- src/test/java/Song99BottlesTest.java
+++ src/test/java/Song99BottlesTest.java
@@ -1,10 +1,22 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class Song99BottlesTest {
+    private static final String BASE_URL = "http://www.99-bottles-of-beer.net/";
+
+    private void getBottles (StringBuilder lyrics, int number, String bottles) {
+        lyrics.append(number).append(bottles);
+    }
+
+    private void getNoMore (StringBuilder lyrics, String noMore, String bottles) {
+        lyrics.append(noMore).append(bottles);
+    }
 
     private String createLyrics() {
         final String BOTTLES_WALL_CS = " bottles of beer on the wall, ";
@@ -15,79 +27,55 @@ public class Song99BottlesTest {
         final String NO_MORE = "No more";
 
         StringBuilder lyrics = new StringBuilder();
-        lyrics
-                .append(99)
-                .append(BOTTLES_WALL_CS)
-                .append(99)
-                .append(BOTTLES_DOT_LN);
+        getBottles(lyrics, 99, BOTTLES_WALL_CS);
+        getBottles(lyrics, 99, BOTTLES_DOT_LN);
 
         for (int i = 98; i > -1; i--) {
+            lyrics.append(TAKE);
+
             if (i == 1) {
-                lyrics
-                        .append(TAKE)
-                        .append(i)
-                        .append(BOTTLES_WALL_DOT.replace("s", ""))
-                        .append(i)
-                        .append(BOTTLES_WALL_CS.replace("s", ""))
-                        .append(i)
-                        .append(BOTTLES_DOT_LN.replace("s", ""));
-
+                getBottles(lyrics, i, BOTTLES_WALL_DOT.replace("s", ""));
+                getBottles(lyrics, i, BOTTLES_WALL_CS.replace("s", ""));
+                getBottles(lyrics, i, BOTTLES_DOT_LN.replace("s", ""));
             } else if (i == 0) {
-                lyrics
-                        .append(TAKE)
-                        .append(NO_MORE.toLowerCase())
-                        .append(BOTTLES_WALL_DOT)
-                        .append(NO_MORE)
-                        .append(BOTTLES_WALL_CS)
-                        .append(NO_MORE.toLowerCase())
-                        .append(BOTTLES_DOT_LN);
-
+                getNoMore(lyrics, NO_MORE.toLowerCase(), BOTTLES_WALL_DOT);
+                getNoMore(lyrics, NO_MORE, BOTTLES_WALL_CS);
+                getNoMore(lyrics, NO_MORE.toLowerCase(), BOTTLES_DOT_LN);
             } else {
-                lyrics
-                        .append(TAKE)
-                        .append(i)
-                        .append(BOTTLES_WALL_DOT)
-                        .append(i)
-                        .append(BOTTLES_WALL_CS)
-                        .append(i)
-                        .append(BOTTLES_DOT_LN);
+                getBottles(lyrics, i, BOTTLES_WALL_DOT);
+                getBottles(lyrics, i, BOTTLES_WALL_CS);
+                getBottles(lyrics, i, BOTTLES_DOT_LN);
             }
         }
-        lyrics
-                .append(GO)
-                .append(99)
-                .append(BOTTLES_WALL_DOT);
+        lyrics.append(GO);
+        getBottles(lyrics, 99, BOTTLES_WALL_DOT);
 
         return lyrics.toString();
     }
 
     @Test
-    public void testSongLyricsText1() {
+    public void testSongLyricsText() {
         String chromeDriver = "webdriver.chrome.driver";
         String driverPath = "/Applications/chromedriver";
-        String url = "http://www.99-bottles-of-beer.net/";
-        String expectedResult = createLyrics();
+        final String expectedResult = createLyrics();
+        By pTags = By.xpath("//div[@id='main']/p");
+        By menuSongLyrics = By.linkText("Song Lyrics");
 
         System.setProperty(chromeDriver, driverPath);
         WebDriver driver = new ChromeDriver();
 
-        driver.get(url);
-        driver.findElement(By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='submenu']/li/" +
-                "a[@href='lyrics.html']")).click();
+        driver.get(BASE_URL);
+        driver.findElement(menuSongLyrics).click();
 
-        String[] textArray = new String[100];
-        for (int i = 0; i < textArray.length; i++) {
-            int index = i + 1;
-            textArray[i] = driver
-                    .findElement(By.xpath("//body/div[@id='wrap']/div[@id='main']/p[" + index + "]")).getText();
+        StringBuilder actualResult = new StringBuilder();
+
+        List<WebElement> allParagraphs = driver.findElements(pTags);
+        Assert.assertFalse(allParagraphs.isEmpty()); // проверка что есть данные (здесь не нужна), просто для примера
+        for(WebElement p : allParagraphs) {
+            actualResult.append(p.getText());
         }
 
-        String actualResult = "";
-        for (int i = 0; i < textArray.length; i++) {
-            actualResult += textArray[i];
-        }
-
-        Assert.assertEquals(actualResult, expectedResult);
+        Assert.assertEquals(actualResult.toString(), expectedResult);
 
         driver.quit();
     }
